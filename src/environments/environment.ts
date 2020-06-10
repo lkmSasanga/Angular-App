b4f6d81366223c70945f1a928b72d71ea3a531e7
lkmSasanga <lkmsasanga@gmail.com>
2020-06-10 12:25:22 +0530

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAlyexsPWippPQerLvwolPJd74xkg5tBvM",
+    authDomain: "todolistapp-e65af.firebaseapp.com",
+    databaseURL: "https://todolistapp-e65af.firebaseio.com",
+    projectId: "todolistapp-e65af",
+    storageBucket: "todolistapp-e65af.appspot.com",
+    messagingSenderId: "225796758804",
+    appId: "1:225796758804:web:596b8142b090e949c17601",
+    measurementId: "G-8J2YYXVZYN"
+  }
 };
 
 /*
